321e7579a2a767b415075e53e3c9b75e9c30fcb5
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2021-11-03 17:37:46 -0300

    Update start.py

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ token = 'your token here'
 bot = telepot.Bot(token)
 
 def receivingMsg(msg):
+    # user says: 
     print(f'@{msg["chat"]["username"]} disse: {msg["text"]}')
 
 bot.message_loop(receivingMsg)
