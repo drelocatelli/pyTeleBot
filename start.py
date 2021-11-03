7632ab64cf06bac8d9be45ebe4564eaaa53c074d
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2021-11-03 17:37:21 -0300

    Update start.py

--- start.py
+++ start.py
@@ -6,10 +6,6 @@ bot = telepot.Bot(token)
 def receivingMsg(msg):
     print(f'@{msg["chat"]["username"]} disse: {msg["text"]}')
 
-    # send msg
-    if(msg["text"] == '/vagas'):
-        bot.sendMessage(msg["chat"]["id"], msg["text"])
-
 bot.message_loop(receivingMsg)
 
 while True:
